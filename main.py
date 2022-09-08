aa42517c1c2e3b3a05d8c140c78eaf9f7c709dc8
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-08 21:35:32 +0200

    Clean Up

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-tiles = {1:1, 2:2, 3:3, 
-         4:4, 5:5, 6:6,
-         7:7, 8:8, 9:9}
+tiles = {'1':1, '2':2, '3':3, 
+         '4':4, '5':5, '6':6,
+         '7':7, '8':8, '9':9}
 
 class Tic_Tac_Toe:
     def __init__(self):
-        print(f" {tiles[1]} | {tiles[2]} | {tiles[3]} "\
+        print(f" {tiles['1']} | {tiles['2']} | {tiles['3']} "\
                 "\n___________\n"\
-                f" {tiles[4]} | {tiles[5]} | {tiles[6]} "\
+                f" {tiles['4']} | {tiles['5']} | {tiles['6']} "\
                 "\n___________\n"\
-                f" {tiles[7]} | {tiles[8]} | {tiles[9]} ")
+                f" {tiles['7']} | {tiles['8']} | {tiles['9']} \n")
 
 class Players:
     def __init__(self, mark):
@@ -16,7 +16,7 @@ class Players:
     
     def move(self,spot):
         if tiles[spot] == "X" or tiles[spot] == "O":
-            print("Invalid Move")
+            print("Invalid Move\n")
         else:
             tiles[spot] = self.mark
         Tic_Tac_Toe()
@@ -28,5 +28,5 @@ player1 = Players("X")
 player2 = Players("O")
 
 while True:         
-    player1.move(int(input("\nPlayer 1: ")))
-    player2.move(int(input("\nPlater 2: ")))
+    player1.move(input("Player 1: "))
+    player2.move(input("Plater 2: "))
